7346ff0efd0766be9d3e63fc9fa432a59d2e6155
LX862 <kenny.mh.hui@outlook.com>
2022-12-04 19:24:56 +0800

    Make a proper website

---
+++ js/404.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    var input = document.getElementById("input");
+    var cursor = document.getElementById("cursor");
+    var workDone = false;
+
+    setInterval(function() {
+        if(cursor.style.display == 'none') {
+            cursor.style.display = ''
+        } else {
+            cursor.style.display = 'none'
+        }
+    }, 250);
+
+    document.onkeypress = function(e) {
+        if(workDone) return;
+        e = e || window.event;
+        var keycode = e.keyCode || e.which;
+
+        if(keycode == 49) {
+            doWork("Redirecting to Home Page...", "1");
+        }
+
+        if(keycode == 50) {
+            doWork("Redirecting to the Internet Archive...", "2")
+        }
+
+        if(keycode == 51) {
+            doWork("Opening email, continue on your email client...", "3")
+        }
+    };
+
+    function doWork(message, key) {
+        workDone = true;
+        var resultHTML = message + '<span id="cursor">_</span>';
+        var resultElement = document.getElementById("result");
+        input.innerHTML = key;
+        cursor.parentNode.removeChild(cursor);
+        resultElement.innerHTML = resultHTML;
+        cursor = document.getElementById("cursor");
+
+        if(key === "1") {
+            window.location.href = "http://" + window.location.host + "/index.html";
+        }
+
+        if(key === "2") {
+            window.location.href = "https://web.archive.org/web/*/" + window.location.href;
+        }
+
+        if(key == "3") {
+            window.location.href = "mailto:address@dmail.com";
+        }
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+window.onload = function() {
+    runClock();
+    setCopyrightYear();
+    setTimeout(checkSpecialDay, 750)
+}
+
+function runClock() {
+    var clockElement = document.getElementById("clock");
+    var date = new Date();
+    clockElement.innerHTML = padZero(date.getHours()) + ":" + padZero(date.getMinutes());
+    setTimeout(runClock, 5000)
+}
+
+function checkSpecialDay() {
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    if(month == 3 && day == 2) {
+        // Cake day!
+        var element = document.getElementById("cakeday");
+        element.style.display = 'block';
+    } else if (month == 12 && (day == 25 || day == 26 || day == 27)) {
+        // Christmas
+        var element = document.getElementById("christmas");
+        element.style.display = 'block';
+    }
+}
+
+function setCopyrightYear() {
+    var element = document.getElementById("copy-year");
+    if(element != null) {
+        element.innerHTML = new Date().getFullYear().toString();
+    }
+}
+
+function padZero(number) {
+    if(number == 0) return "00";
+    if(number <= 9) return "0" + number;
+    return number;
+}
